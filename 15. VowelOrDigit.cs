bb5886ba44ac4534f18339697e313f64820f07c4
Irina Stefanova Yordanova <irina_stz@abv.bg>
2016-09-26 12:53:16 +0300

    Add files via upload

---
+++ 15. VowelOrDigit.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15.VowelOrDigit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char symbol = char.Parse(Console.ReadLine());
+
+            bool isDigit = (Char.IsDigit(symbol));
+            bool isVowel = "aeiouAEIOU".IndexOf(symbol)>=0;
+    
+            if (isDigit)
+                Console.WriteLine("digit");
+            else if(isVowel)
+                Console.WriteLine("vowel");
+            else
+            {
+                Console.WriteLine("other");
+            }
+        }
+    }
+}
