60603766c6857e0e101cdc18f0a154c8da4cda65
Irina Stefanova Yordanova <irina_stz@abv.bg>
2016-09-26 12:55:13 +0300

    Add files via upload

---
+++ 18. ComparingFloats.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18.ComparingFloats
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           decimal num1 = decimal.Parse(Console.ReadLine());
+            decimal num2 = decimal.Parse(Console.ReadLine());
+            decimal diff = Math.Abs(num1 - num2);
+            decimal eps = 0.000001m;
+
+            if (diff>=eps)
+            {
+                Console.WriteLine("False");
+            }
+            else
+            {
+                Console.WriteLine("True");
+            }
+           
+        }
+    }
+}
